c4b646d416da3b78b9f8ac9c634fc81841469b08
Luke Fan <lukefan@oath.com>
2017-11-01 13:35:19 +0800

    Finish next random date

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,28 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
 <script>
+import * as moment from 'moment';
+
+function getNextRandomNumber(start, end) {
+  return Math.floor((Math.random() * end) + start);
+}
+
+function getNextRandomDate(baseline, daysToAdd) {
+  const nextAddedHours = getNextRandomNumber(0, daysToAdd * 24);
+
+  return moment(baseline).add(nextAddedHours, 'hours');
+}
+
 export default {
   name: 'HelloWorld',
   data() {
+    const date = getNextRandomDate(new Date(), 7);
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: moment(date).format('YYYY-MM-DD HH:00'),
     };
   },
 };
@@ -33,7 +30,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
