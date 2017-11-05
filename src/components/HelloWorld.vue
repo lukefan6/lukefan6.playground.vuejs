5bd3157f2e5a07e627e9e163c1798a765e0d7236
Luke Fan <lukefan@oath.com>
2017-11-05 14:49:47 +0800

    Add bootstrap-vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col offset-lg="2" lg="8">
+          <b-input-group>
+            <b-form-input v-model="daysToAdd" type="number"></b-form-input>
+            <b-input-group-button slot="right">
+              <b-button variant="success" v-on:click="runAgain">
+                Run
+              </b-button>
+            </b-input-group-button>
+          </b-input-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col offset-lg="2" lg="8">
+          <hr>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col offset-lg="2" lg="8">
+          <b-alert variant="info" dismissible :show="showDismissibleAlert" @dismissed="showDismissibleAlert=false">
+            <h1>{{ msg }}</h1>
+          </b-alert>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -8,7 +33,8 @@
 import * as moment from 'moment';
 
 function getNextRandomNumber(start, end) {
-  return Math.floor((Math.random() * end) + start);
+  const base = Math.random() * end;
+  return Math.floor(base + start);
 }
 
 function getNextRandomDate(baseline, daysToAdd) {
@@ -20,17 +46,26 @@ function getNextRandomDate(baseline, daysToAdd) {
 export default {
   name: 'HelloWorld',
   data() {
-    const date = getNextRandomDate(new Date(), 7);
     return {
-      msg: moment(date).format('YYYY-MM-DD HH:00'),
+      msg: '',
+      daysToAdd: 7,
+      showDismissibleAlert: false,
     };
+  },
+  // define methods under the `methods` object
+  methods: {
+    runAgain() {
+      const date = getNextRandomDate(new Date(), this.daysToAdd);
+      this.msg = moment(date).format('YYYY-MM-DD HH:00');
+      this.showDismissibleAlert = true;
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
+/* h1,
 h2 {
   font-weight: normal;
 }
@@ -47,5 +82,5 @@ li {
 
 a {
   color: #42b983;
-}
+} */
 </style>
